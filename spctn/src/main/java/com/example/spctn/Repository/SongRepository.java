f56a307708079360c1347707e4416d87157d33c7
khalid <rabaaouikhalid@gmail.com>
2026-07-31 13:57:55 +0200

    update2

--- spctn/src/main/java/com/example/spctn/Repository/SongRepository.java
+++ spctn/src/main/java/com/example/spctn/Repository/SongRepository.java
@@ -8,17 +8,23 @@ import java.util.Optional;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
 public interface SongRepository extends JpaRepository<Song, Long> {
-	
+	@EntityGraph(attributePaths = {"category"})
 	Optional<Song> findByTitulo(String titulo);
 	
+	@EntityGraph(attributePaths = {"category"})
 	List<Song> findByCategoryId(Long CategoryId);
 	
+	@EntityGraph(attributePaths = {"category"})
 	boolean existsByTitulo(String titulo);
 	
+	@EntityGraph(attributePaths = {"category"})
 	Page<Song> findByTituloContainingIgnoreCase(String titulo, Pageable pageable);
+	
+	@EntityGraph(attributePaths = {"category"})
 	Page<Song> findByCategoryId(Long categoryId, Pageable pageable);
 }
