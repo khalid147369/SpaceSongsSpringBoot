913c86bb01eb8af906f0d626ab4d955d7f50883c
khalid <rabaaouikhalid@gmail.com>
2026-08-30 15:50:33 +0200

    edit title size

--- spctn/src/main/java/com/example/spctn/Dto/Request/SongRequestDTO.java
+++ spctn/src/main/java/com/example/spctn/Dto/Request/SongRequestDTO.java
@@ -16,7 +16,7 @@ import lombok.Setter;
 public class SongRequestDTO {
 
 	@NotBlank
-	@Size(max = 60, message = "The title must not exceed 60 characters")
+	@Size(max = 120, message = "The title must not exceed 60 characters")
     private String title;
     
     @NotBlank
